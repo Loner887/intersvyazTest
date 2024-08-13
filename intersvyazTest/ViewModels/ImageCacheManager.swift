f38fc637a4e60c285b535034c15ec2b261dbe954
Loner887 <100552193+Loner887@users.noreply.github.com>
2024-08-13 09:32:09 +0500

    Update ImageCacheManager.swift

--- intersvyazTest/ViewModels/ImageCacheManager.swift
+++ intersvyazTest/ViewModels/ImageCacheManager.swift
@@ -8,23 +8,19 @@ class ImageCacheManager {
     private init() {}
     
     func loadImage(from url: URL, completion: @escaping (UIImage?) -> Void) {
-        // Check if the image is already cached
         if let cachedImage = cache.object(forKey: url.absoluteString as NSString) {
             completion(cachedImage)
             return
         }
         
-        // Download the image if it's not cached
         URLSession.shared.dataTask(with: url) { data, response, error in
             guard let data = data, let image = UIImage(data: data), error == nil else {
                 completion(nil)
                 return
             }
             
-            // Cache the image
             self.cache.setObject(image, forKey: url.absoluteString as NSString)
             
-            // Return the image
             DispatchQueue.main.async {
                 completion(image)
             }
